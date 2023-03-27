5bfe69b040313c5c9c52293ea67f3a9d8da945eb
OksanaKarpyn <93349703+OksanaKarpyn@users.noreply.github.com>
2023-03-27 22:35:32 +0200

    agg dado gioco

--- gioco-dadi/assets/js/main.js
+++ gioco-dadi/assets/js/main.js
@@ -12,19 +12,24 @@ Stabilire il vincitore, in base a chi fa il punteggio più alto. */
 
 
 // function random
-let number = document.querySelector('#number');
-function random(min, max) {
-    return Math.floor(Math.random() * max) + min;
-    
-}
-let giocatore = random(1, 6);
-let computer = random(1, 6);
-// console.log(giocatore > computer ? 'vincitore e il giocatore' : 'vincitore e il  computer');
 
-if(giocatore > computer){
-    number.innerHTML=`${random}`;
+// function random(min, max) {
+//  return Math.floor(Math.random() * max) + min;
+// }
+
+// let giocatore = random(1, 6);
+// let computer = random(1, 6);
+// // console.log(giocatore > computer ? 'vincitore e il giocatore' : 'vincitore e il  computer');
+
+let number = document.querySelector('#number');
+
+function random() {
+
+    let giocatore = Math.floor(Math.random() * 6) + 1;
+    let computer = Math.floor(Math.random() * 6) + 1;
+    if (giocatore > computer) {
+        number.innerHTML = `vincitore è il giocatore con${giocatore} punti`;
+    } else {
+        number.innerHTML = `vincitore è il computer con${computer} punti`;
+    }
 }
-else{
-    number.innerHTML=`${random}`;
-}
-console.log();
